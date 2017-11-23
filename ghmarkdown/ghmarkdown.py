8e4c937c7f2dc45892ace4623a1354c0e5d63cb3
Lucas Morales <lucas@lucasem.com>
2017-11-23 15:28:00 -0600

    move into directory for package_data

--- ghmarkdown/ghmarkdown.py
+++ ghmarkdown/ghmarkdown.py
@@ -12,7 +12,7 @@ import sys
 import os
 import requests
 
-__version__ = "2.0.1"
+__version__ = "2.0.2"
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 
-version = '2.0.1'
+version = '2.0.2'
 
 setup(
     name='ghmarkdown',
@@ -39,7 +39,7 @@ setup(
     install_requires=['requests'],
     entry_points={
         'console_scripts': [
-            'ghmarkdown=ghmarkdown:main'
+            'ghmarkdown=ghmarkdown.ghmarkdown:main'
         ],
     }
 )
